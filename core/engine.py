42591858c71d3310b44e74a6950238acb567a853
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-04 19:51:21 -0500

    Add 'CE' role to ROLES tuple in engine.py

--- core/engine.py
+++ core/engine.py
@@ -118,7 +118,7 @@ class ContratoError(Exception):
 # ===============================================================
 ROLES: Tuple[str, ...] = (
     "CT", "AX", "FO", "MC", "SF", "DG", "CA", "CX", "DI",
-    "RE", "VX", "TX", "CH", "CIT", "UI", "GL", "TT", "CC",
+    "RE", "VX", "TX", "CH", "CIT", "UI", "GL", "TT", "CC", "CE",
 )
 OBLIGATORIOS: Tuple[str, ...] = ("CT", "AX", "FO", "MC", "SF")
 
