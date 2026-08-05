50c9dea9191091a4048277410d4fef3464570091
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-04 22:37:42 -0500

    Add ids_mandato.py with mandate IDs

---
+++ modules/catalogo_citaciones/ids_mandato.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""IDs de mandato CE. Solo ids."""
+
+IDS = [
+    "ce_mandato_catalogo",
+    "ce_mandato_escala_tt",
+    "ce_mandato_sujetos",
+    "ce_mandato_aplicar_escala",
+]
