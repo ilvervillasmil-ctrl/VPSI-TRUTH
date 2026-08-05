8cabe883a596c2825503d2ed50abf67796351e4c
Ilver Villasmil <ilvervillasmil@gmail.com>
2026-08-04 19:09:44 -0500

    Add 'CC' role to the ROLES tuple

--- core/engine.py
+++ core/engine.py
@@ -118,7 +118,7 @@ class ContratoError(Exception):
 # ===============================================================
 ROLES: Tuple[str, ...] = (
     "CT", "AX", "FO", "MC", "SF", "DG", "CA", "CX", "DI",
-    "RE", "VX", "TX", "CH", "CIT", "UI", "GL", "TT",
+    "RE", "VX", "TX", "CH", "CIT", "UI", "GL", "TT", "CC",
 )
 OBLIGATORIOS: Tuple[str, ...] = ("CT", "AX", "FO", "MC", "SF")
 
